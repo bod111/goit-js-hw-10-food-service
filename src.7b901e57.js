4460ebfda384d5b6a430a8eae507aa4dbfcd95f3
bod111 <86015208+bod111@users.noreply.github.com>
2021-08-28 17:09:56 +0000

    Deploying to gh-pages from @ bod111/hw_10@242561459ee4de7cd53c10c982efcfd1748d00ea 🚀

--- src.7b901e57.js
+++ src.7b901e57.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/hw_test/src.45dd0ea9.js.map
+//# sourceMappingURL=/hw_10/src.7b901e57.js.map
