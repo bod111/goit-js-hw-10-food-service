11eba7f233fda659c71d1675e506f57e9ef190f2
bod111 <86015208+bod111@users.noreply.github.com>
2021-09-05 15:41:06 +0300

    rev 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import './sass/main.scss';
+import DATA from './menu.json';
+import foodCards from './templates/foodCards.hbs';
+const refs = {
+  menuRef: document.querySelector('.js-menu'),
+  bodyColor: document.querySelector('body'),
+  themeBtn: document.querySelector('#theme-switch-toggle'),
+  ligthTheme: 'light-theme',
+  darkTheme: 'dark-theme',
+};
+const menuMarkup = foodCards(DATA);
+refs.menuRef.innerHTML = menuMarkup;
+
+refs.themeBtn.addEventListener('change', onThemeSwitch);
+
+function onThemeSwitch(evt) {
+  const themeColor = evt.target.checked ? refs.darkTheme : refs.ligthTheme;
+  console.log('onThemeSwitch ~ themeColor', themeColor);
+  console.log('onThemeSwitch ~ evt.target.checked', evt.target.checked);
+
+  setData(themeColor);
+  setBodyColor(themeColor);
+}
+// запись темы body
+function setData(data) {
+  localStorage.setItem('theme', data);
+}
+// запись/перезапись класса темы body
+function setBodyColor(classColor) {
+  refs.bodyColor.className = classColor;
+  console.log('setBodyColor ~ classColor', classColor);
+}
+
+function changeCheckboxTheme() {
+  const firstData = localStorage.getItem('theme') || refs.ligthTheme;
+  console.log('changeCheckboxTheme ~ firstData', firstData);
+  setBodyColor(firstData);
+
+  refs.themeBtn.checked = firstData === refs.darkTheme;
+}
+changeCheckboxTheme();
